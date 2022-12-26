393569a0c9adeab48f324067d94e10c69941e378
Max! <admin@cloudydev.net>
2022-12-25 16:59:38 -0800

    Create gke.tf

---
+++ gke.tf
@@ -0,0 +1,39 @@
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/container_cluster
+# Basic GKE Kubernetes cluster utilizing the default node pool.
+
+resource "google_container_cluster" "container_cluster" {
+  provider                 = google-beta
+  name                     = var.cluster_name
+  location                 = var.region
+  subnetwork               = data.google_compute_subnetwork.compute_subnetwork.id
+  remove_default_node_pool = var.use_default_node_pool
+  initial_node_count       = var.initial_node_count
+
+  node_config {
+    disk_type    = var.disk_type
+    machine_type = var.machine_type
+  }
+
+  cluster_autoscaling {
+    enabled             = var.autoscaling_enabled
+    autoscaling_profile = var.autoscaling_strategy
+    resource_limits {
+      resource_type = "memory"
+      minimum       = var.autoscaling_min_mem
+      maximum       = var.autoscaling_max_mem
+
+    }
+    resource_limits {
+      resource_type = "cpu"
+      minimum       = var.autoscaling_min_cpu
+      maximum       = var.autoscaling_max_cpu
+    }
+    auto_provisioning_defaults {
+      service_account = var.node_service_account
+    }
+  }
+}
+
+output "endpoint" {
+  value = google_container_cluster.container_cluster.endpoint
+}
