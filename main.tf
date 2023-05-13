4284f65cdffa705a5aae0dc3c178a06549c27caf
Max! <admin@cloudydev.net>
2023-05-13 20:54:17 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@
 # Basic GKE Kubernetes cluster utilizing the default node pool.
 
 resource "google_container_cluster" "container_cluster" {
+  project                  = var.project
   provider                 = google-beta
   name                     = var.cluster_name
   location                 = var.region
