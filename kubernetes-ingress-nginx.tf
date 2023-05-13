9f829c6f80d2f891c9f6a517a8443109b3937c91
Max! <admin@cloudydev.net>
2023-05-13 21:50:31 +0200

    Update kubernetes-ingress-nginx.tf

--- kubernetes-ingress-nginx.tf
+++ kubernetes-ingress-nginx.tf
@@ -6,6 +6,16 @@ provider "kubernetes" {
   )
 }
 
+provider "helm" {
+  kubernetes {
+    host  = "https://${data.google_container_cluster.cluster.endpoint}"
+    token = data.google_client_config.provider.access_token
+    cluster_ca_certificate = base64decode(
+      data.google_container_cluster.container_cluster.master_auth[0].cluster_ca_certificate
+    )
+  }
+}
+
 resource "helm_release" "nginx_ingress" {
   name       = "ingress-nginx"
   namespace  = "ingress-nginx"
