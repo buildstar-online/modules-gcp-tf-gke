dd180578611ea11968d9c081c2369d170e59707d
Max! <admin@cloudydev.net>
2023-05-13 21:54:16 +0200

    Update kubernetes-ingress-nginx.tf

--- kubernetes-ingress-nginx.tf
+++ kubernetes-ingress-nginx.tf
@@ -1,17 +1,17 @@
 provider "kubernetes" {
-  host  = "https://${data.google_container_cluster.container_cluster.endpoint}"
+  host  = "https://${google_container_cluster.container_cluster.endpoint}"
   token = data.google_client_config.provider.access_token
   cluster_ca_certificate = base64decode(
-    data.google_container_cluster.container_cluster.master_auth[0].cluster_ca_certificate,
+    google_container_cluster.container_cluster.master_auth[0].cluster_ca_certificate,
   )
 }
 
 provider "helm" {
   kubernetes {
-  host  = "https://${data.google_container_cluster.container_cluster.endpoint}"
+  host  = "https://${google_container_cluster.container_cluster.endpoint}"
     token = data.google_client_config.provider.access_token
     cluster_ca_certificate = base64decode(
-      data.google_container_cluster.container_cluster.master_auth[0].cluster_ca_certificate
+      google_container_cluster.container_cluster.master_auth[0].cluster_ca_certificate
     )
   }
 }
