3e4f2f89e4fd3412af9f6e656a84c7bb2e1bc853
Max! <admin@cloudydev.net>
2023-05-22 21:51:14 +0200

    Update apps.tf

--- apps.tf
+++ apps.tf
@@ -9,24 +9,6 @@ resource "kubectl_manifest" "nvidia_driver_installer" {
   yaml_body = data.http.nvidia_driver_installer_manifest.body
 }
 
-provider "kubernetes" {
-  host  = "https://${google_container_cluster.container_cluster.endpoint}"
-  token = data.google_client_config.provider.access_token
-  cluster_ca_certificate = base64decode(
-    google_container_cluster.container_cluster.master_auth[0].cluster_ca_certificate,
-  )
-}
-
-provider "helm" {
-  kubernetes {
-  host  = "https://${google_container_cluster.container_cluster.endpoint}"
-    token = data.google_client_config.provider.access_token
-    cluster_ca_certificate = base64decode(
-      google_container_cluster.container_cluster.master_auth[0].cluster_ca_certificate
-    )
-  }
-}
-
 resource "helm_release" "nginx_ingress" {
   name             = "ingress-nginx"
   namespace        = "ingress-nginx"
