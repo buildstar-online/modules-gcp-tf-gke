56721e3dbd4a276fde0d62da6c39f3be6db3e1a4
Max! <admin@cloudydev.net>
2023-05-22 22:11:03 +0200

    Update apps.tf

--- apps.tf
+++ apps.tf
@@ -1,6 +1,7 @@
 data "google_client_config" "provider" {
 }
 
+/*
 data "http" "nvidia_driver_installer_manifest" {
   url = "https://raw.githubusercontent.com/GoogleCloudPlatform/container-engine-accelerators/master/nvidia-driver-installer/ubuntu/daemonset-preloaded.yaml"
 }
@@ -8,7 +9,8 @@ data "http" "nvidia_driver_installer_manifest" {
 resource "kubernetes_manifest" "nvidia_driver_installer" {
   manifest = yamldecode(data.http.nvidia_driver_installer_manifest.response_body)
 }
-
+*/
+  
 resource "helm_release" "nginx_ingress" {
   name             = "ingress-nginx"
   namespace        = "ingress-nginx"
