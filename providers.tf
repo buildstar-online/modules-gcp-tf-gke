12cbb344061f16dfdfa33b8d53e92986fdf0ea98
Max! <admin@cloudydev.net>
2023-05-22 20:50:11 +0200

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,15 +2,15 @@ terraform {
   required_providers {
     helm = {
       source = "hashicorp/helm"
-      version = "2.9.0"
+      version = ">=2.9.0"
     }
     kubernetes = {
       source = "hashicorp/kubernetes"
-      version = "2.20.0"
+      version = ">=2.20.0"
     }
     google-beta = {
       source = "hashicorp/google-beta"
-      version = "3.67.0"
+      version = ">=4.47.0"
     }
   }
 }
