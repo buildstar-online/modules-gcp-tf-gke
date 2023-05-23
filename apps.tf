0e7f0711a49b03f9cbeba5441f0d52c8376b2a57
Max! <admin@cloudydev.net>
2023-05-23 21:24:12 +0200

    Update apps.tf

--- apps.tf
+++ apps.tf
@@ -2,6 +2,25 @@ data "google_client_config" "provider" {
 }
 
 /*
+provider "kubernetes" {
+  host  = "https://${google_container_cluster.container_cluster.endpoint}"
+  token = data.google_client_config.provider.access_token
+  cluster_ca_certificate = base64decode(
+    google_container_cluster.container_cluster.master_auth[0].cluster_ca_certificate,
+  )
+}
+
+provider "helm" {
+  kubernetes {
+  host  = "https://${google_container_cluster.container_cluster.endpoint}"
+    token = data.google_client_config.provider.access_token
+    cluster_ca_certificate = base64decode(
+      google_container_cluster.container_cluster.master_auth[0].cluster_ca_certificate
+    )
+  }
+}
+
+
 data "http" "nvidia_driver_installer_manifest" {
   url = "https://raw.githubusercontent.com/GoogleCloudPlatform/container-engine-accelerators/master/nvidia-driver-installer/ubuntu/daemonset-preloaded.yaml"
 }
