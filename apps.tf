c70d53b2610bceb46d7af7e9c7cee5e83661529c
Max! <admin@cloudydev.net>
2023-05-23 19:55:05 +0200

    Update apps.tf

--- apps.tf
+++ apps.tf
@@ -11,10 +11,38 @@ resource "kubernetes_manifest" "nvidia_driver_installer" {
 }
 */
 
+resource "kubernetes_manifest" "cluster-issuer" {
+  manifest = yamldecode(<<-EOF
+    apiVersion: cert-manager.io/v1
+    kind: ClusterIssuer
+    metadata:
+      name: letsencrypt-staging
+    spec:
+      acme:
+        server: https://acme-staging-v02.api.letsencrypt.org/directory
+        email: admin@cloudydev.net
+        privateKeySecretRef:
+          name: letsencrypt-staging
+        solvers:
+        - http01:
+            ingress:
+              class: nginx
+    EOF
+  )
+}
+
 resource "helm_release" "nginx_ingress" {
   name             = "ingress-nginx"
   namespace        = "ingress-nginx"
   create_namespace = true
   repository       = "https://kubernetes.github.io/ingress-nginx"
   chart            = "ingress-nginx"
+}
+
+resource "helm_release" "cert_manager" {
+  name             = "cert-manager"
+  namespace        = "kube-system"
+  create_namespace = true
+  repository       = "https://charts.jetstack.io"
+  chart            = "cert-manager"
 }
