4a02b3af88fcf119658670f98e7428b640be4789
Max! <admin@cloudydev.net>
2023-05-23 21:23:47 +0200

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -14,21 +14,3 @@ terraform {
     }
   }
 }
-
-provider "kubernetes" {
-  host  = "https://${google_container_cluster.container_cluster.endpoint}"
-  token = data.google_client_config.provider.access_token
-  cluster_ca_certificate = base64decode(
-    google_container_cluster.container_cluster.master_auth[0].cluster_ca_certificate,
-  )
-}
-
-provider "helm" {
-  kubernetes {
-  host  = "https://${google_container_cluster.container_cluster.endpoint}"
-    token = data.google_client_config.provider.access_token
-    cluster_ca_certificate = base64decode(
-      google_container_cluster.container_cluster.master_auth[0].cluster_ca_certificate
-    )
-  }
-}
